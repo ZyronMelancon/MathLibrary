a754fbd5c3416b8b47828a56651b59582a291f2d
ZyronMelancon <altausolaris@gmail.com>
2016-11-22 16:26:58 -0600

    Near-Completion
    Documentation has been added and it almost done. Added operator
    overloads to Vector classes for multiplication by Matrices.

--- MathLibrary/Math.cpp
+++ MathLibrary/Math.cpp
@@ -24,7 +24,7 @@ float Vector2::magnitude()
 
 void Vector2::normalise()
 {
-	if (m_x > m_y) //Checks which axis has the highest value
+	if (m_x > m_y)
 	{
 		m_y = m_y / m_x;
 		m_x = 1;
@@ -41,19 +41,20 @@ void Vector2::print()
 	std::cout << m_x << ", " << m_y << std::endl;
 }
 
-Vector2 Vector2::operator+(Vector2 & other)
+Vector2 Vector2::operator+(Vector2 other)
 {
 	return Vector2(m_x + other.m_x, m_y + other.m_y);
 }
 
-Vector2 Vector2::operator-(Vector2 & other)
+Vector2 Vector2::operator-(Vector2 other)
 {
 	return Vector2(m_x - other.m_x, m_y - other.m_y);
 }
 
-Vector2 Vector2::operator*(Vector2 & other)
+Vector2 Vector2::operator*(Matrix2 other)
 {
-	return Vector2(m_x * other.m_x, m_y * other.m_y);
+	return Vector2(m_x * other.m_a.m_x + m_y * other.m_a.m_y,
+		m_x * other.m_b.m_x + m_y * other.m_b.m_y);
 }
 
 Vector2 Vector2::operator*(float m)
@@ -87,7 +88,9 @@ float Vector3::dot(Vector3 other)
 
 Vector3 Vector3::cross(Vector3 other)
 {
-	return Vector3(m_y * other.m_z - m_z * other.m_y, m_x * other.m_z - m_z * other.m_x, m_x * other.m_y - m_y * other.m_x);
+	return Vector3(m_y * other.m_z - m_z * other.m_y,
+		m_x * other.m_z - m_z * other.m_x,
+		m_x * other.m_y - m_y * other.m_x);
 }
 
 float Vector3::magnitude()
@@ -122,19 +125,21 @@ void Vector3::print()
 	std::cout << m_x << ", " << m_y << ", " << m_z << std::endl;
 }
 
-Vector3 Vector3::operator+(Vector3 & other)
+Vector3 Vector3::operator+(Vector3 other)
 {
 	return Vector3(m_x + other.m_x, m_y + other.m_y, m_z + other.m_z);
 }
 
-Vector3 Vector3::operator-(Vector3 & other)
+Vector3 Vector3::operator-(Vector3 other)
 {
 	return Vector3(m_x - other.m_x, m_y - other.m_y, m_z - other.m_z);
 }
 
-Vector3 Vector3::operator*(Vector3 & other)
+Vector3 Vector3::operator*(Matrix3 other)
 {
-	return Vector3(m_x * other.m_x, m_y * other.m_y, m_z * other.m_z);
+	return Vector3(m_x * other.m_a.m_x + m_y * other.m_a.m_y + m_z * other.m_a.m_z, 
+		m_x * other.m_b.m_x + m_y * other.m_b.m_y + m_z * other.m_b.m_z, 
+		m_x * other.m_c.m_x + m_y * other.m_c.m_y + m_z * other.m_c.m_z);
 }
 
 Vector3 Vector3::operator*(float m)
@@ -215,19 +220,22 @@ void Vector4::print()
 	std::cout << m_x << ", " << m_y << ", " << m_z << ", " << m_w << std::endl;
 }
 
-Vector4 Vector4::operator+(Vector4 & other)
+Vector4 Vector4::operator+(Vector4 other)
 {
 	return Vector4(m_x + other.m_x, m_y + other.m_y, m_z + other.m_z, m_w + other.m_w);
 }
 
-Vector4 Vector4::operator-(Vector4 & other)
+Vector4 Vector4::operator-(Vector4 other)
 {
 	return Vector4(m_x - other.m_x, m_y - other.m_y, m_z - other.m_z, m_w - other.m_w);
 }
 
-Vector4 Vector4::operator*(Vector4 & other)
+Vector4 Vector4::operator*(Matrix4 other)
 {
-	return Vector4(m_x * other.m_x, m_y * other.m_y, m_z * other.m_z, m_w * other.m_w);
+	return Vector4(m_x * other.m_a.m_x + m_y * other.m_a.m_y + m_z * other.m_a.m_z + m_w * other.m_a.m_w,
+		m_x * other.m_b.m_x + m_y * other.m_b.m_y + m_z * other.m_b.m_z + m_w * other.m_b.m_w,
+		m_x * other.m_c.m_x + m_y * other.m_c.m_y + m_z * other.m_c.m_z + m_w * other.m_c.m_w,
+		m_x * other.m_d.m_x + m_y * other.m_d.m_y + m_z * other.m_d.m_z + m_w * other.m_d.m_w);
 }
 
 Vector4 Vector4::operator*(float m)
--- MathLibrary/Math.h
+++ MathLibrary/Math.h
@@ -16,9 +16,9 @@ public:
 	float magnitude();							//Magnitude calculator
 	void normalise();							//Normalise function
 	void print();								//Prints to console in x, y, z, w format
-	Vector4 operator + (Vector4 & other);		
-	Vector4 operator - (Vector4 & other);		//All mathematical operator overloads
-	Vector4 operator * (Vector4 & other);
+	Vector4 operator + (Vector4 other);		
+	Vector4 operator - (Vector4 other);			//All mathematical operator overloads
+	Vector4 operator * (Matrix4 other);
 	Vector4 operator * (float m);
 	Vector4 operator / (float d);
 };
@@ -37,9 +37,9 @@ public:
 	float magnitude();							//Magnitude calculator
 	void normalise();							//Normalise function
 	void print();								//Prints to console in x, y, z format
-	Vector3 operator + (Vector3 & other);
-	Vector3 operator - (Vector3 & other);		//All mathematical operator overloads
-	Vector3 operator * (Vector3 & other);
+	Vector3 operator + (Vector3 other);
+	Vector3 operator - (Vector3 other);			//All mathematical operator overloads
+	Vector3 operator * (Matrix3 other);
 	Vector3 operator * (float m);
 	Vector3 operator / (float d);
 };
@@ -56,19 +56,19 @@ public:
 	float magnitude();							//Magnitude calculator
 	void normalise();							//Normalize function
 	void print();								//Prints to console in x, y format
-	Vector2 operator + (Vector2 & other);
-	Vector2 operator - (Vector2 & other);		//All mathematical operator overloads
-	Vector2 operator * (Vector2 & other);
+	Vector2 operator + (Vector2 other);
+	Vector2 operator - (Vector2 other);			//All mathematical operator overloads
+	Vector2 operator * (Matrix2 other);
 	Vector2 operator * (float m);
 	Vector2 operator / (float d);
 };
 
 class Matrix2									//2 x 2 rotation Matrix
 {
-private:
+public:
 	Vector2 m_a;								//Row 1
 	Vector2 m_b;								//Row 2
-public:
+
 	Matrix2() {}								//Default constructor
 	Matrix2(Vector2 a, Vector2 b);				//Takes in two Vector2s
 	Matrix2(float a, float b, float c, float d);//Takes in 4 floating point numbers
@@ -78,11 +78,11 @@ public:
 
 class Matrix3									//3 x 3 rotation Matrix
 {
-private:
+public:
 	Vector3 m_a;								//Row 1
 	Vector3 m_b;								//Row 2
 	Vector3 m_c;								//Row 3
-public:
+
 	Matrix3() {}								//Default constructor
 	Matrix3(Vector3 a, Vector3 b, Vector3 c);	//Takes in 3 Vector3s
 	Matrix3(float a, float b, float c, float d, 
@@ -90,18 +90,18 @@ public:
 	void Print();								//Prints to console in a 3 x 3 grid format
 	Matrix3 operator * (Matrix3 o);				//Operator overload for multiplication of similar matrices
 	void setRotatex(float r);
-	void setRotateY(float r);
+	void setRotateY(float r);					//Set rotation on any axis
 	void setRotateZ(float r);
 };
 
 class Matrix4									//4 x 4 transform Matrix
 {
-private:
+public:
 	Vector4 m_a;								//Row 1
 	Vector4 m_b;								//Row 2
 	Vector4 m_c;								//Row 3
 	Vector4 m_d;								//Row 4
-public:
+
 	Matrix4() {}								//Default constructor
 	Matrix4(Vector4 a, Vector4 b, Vector4 c, Vector4 d);//Takes in 4 Vector4s
 	Matrix4(float a, float b, float c, float d,	//Takes in 16 individual floats
@@ -110,6 +110,6 @@ public:
 		float m, float n, float o, float p);
 	Matrix4 operator * (Matrix4 o);				//Operator overload for multiplication of similar matrices
 	void setRotateX(float r);
-	void setRotateY(float r);
+	void setRotateY(float r);					//Set rotation on any axis
 	void setRotateZ(float r);
 };
